e99dac13130fec74c180c0dd377fb2234f7959ab
vipulrawat <vipulrawat.imi@live.in>
2019-12-31 06:06:07 +0530

    adds multiply

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ function add(firstNumber, secondNumber) {
   return firstNumber + secondNumber;
 }
 
+function multiply(firstNumber) {
+  return function(secondNumber) {
+    return firstNumber * secondNumber;
+  }
+}
+
 module.exports = {
-  add
+  add,
+  multiply
 }
--- index.test.js
+++ index.test.js
@@ -1,5 +1,9 @@
-const { add } = require('./index');
+const { add, multiply } = require('./index');
 
 it('should add', function() {
   expect(add(1, 2)).toBe(3);
 });
+
+it('should multiply', function() {
+  expect(multiply(1)(2)).toBe(2);
+})
