51c7f20135f1b838d2127338fae355b6cfe2f1e7
Vipul <vipulrawat.imi@live.in>
2020-01-01 04:27:46 +0530

    powerrrrrrr (#3)

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ function multiply(firstNumber) {
   }
 }
 
+function pow(number, power) {
+  return Math.pow(number, power);
+}
+
 module.exports = {
   add,
-  multiply
+  multiply,
+  pow
 }
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const { add, multiply } = require('./index');
+const { add, multiply, pow } = require('./index');
 
 it('should add', function() {
   expect(add(1, 2)).toBe(3);
@@ -6,4 +6,8 @@ it('should add', function() {
 
 it('should multiply', function() {
   expect(multiply(1)(2)).toBe(2);
+})
+
+it('should power', function() {
+  expect(pow(3, 3)).toBe(27);
 })
