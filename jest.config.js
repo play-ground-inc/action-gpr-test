a19eb80af64b8f63b189749e8a94cb528326d0b0
vipulrawat <vipulrawat.imi@live.in>
2020-02-05 22:36:24 +0530

    added setupFilesEnv

--- jest.config.js
+++ jest.config.js
@@ -10,4 +10,5 @@ module.exports = {
     },
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    setupFilesAfterEnv: ["jest-extended"]
   }
